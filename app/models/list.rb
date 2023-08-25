9648bdc1ae1b29f787d075c6d539ac416f1f0978
Cedric Garcia <cgdm@tutanota.com>
2023-08-25 16:05:44 +0200

    Heyo

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ApplicationRecord
-  has_many :bookmarks, dependent: delete
-  has_many :movies, through :bookmarks
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks
 
-  validates :name, uniqueness: true
+  validates :name, presence: true, uniqueness: true
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,12 +3,4 @@ class Movie < ApplicationRecord
 
   validates :title, :overview, presence: true
   validates :title, uniqueness: true
-
-  before_destroy :check_if_bookmarked
-
-  private
-
-  def check_if_bookmarked
-
-  end
 end
