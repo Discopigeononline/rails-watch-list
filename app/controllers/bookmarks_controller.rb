64057d6efc77f008297abd7ef76cb33eabec99db
Cedric Garcia <cgdm@tutanota.com>
2023-08-27 19:53:30 +0200

    tehr

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,16 @@
 class BookmarksController < ApplicationController
+  before_action :set_list
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.create
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
